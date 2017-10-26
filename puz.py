1a298e938e1a253f72323cceef415f06cada28c3
Marco Mercado <marco_mercado@outlook.com>
2017-10-26 16:32:46 -0700

    started the classes and the uniform function

--- puz.py
+++ puz.py
@@ -1,6 +1,22 @@
 import sys
 import numpy as np
+import copy
+def parent(matrix, cost):
 
+    def __init__(self, matrix, cost):
+          self.M = matrix
+          self.C = cost
+          self.STATE = None
+    def state(self,newState):
+        self.STATE = newState;
+        
+
+
+
+
+
+def Unifor_Cost_Search(problem):
+    child = copy.deepcopy(problem)
 
 
 if __name__ == "__main__":
@@ -8,7 +24,15 @@ if __name__ == "__main__":
     print("Welcome to Bertie Woosters 8-puzzle solver. ")
     ValInput = input("Type \"1\" to use a default puzzle, or \"2\" to enter your own puzzle\n" )
     if ValInput ==  1:
-        problem = np.matrix([[1,2,3], [4,5,6], [7.8,0]])
+        m = np.matrix([[1,2,3], [4,5,6], [7.8,0]])
+        problem = parent(m,0)
+        print("Enter your choice of algorithm")
+        print("1. Uniform Cost Search")
+        print("2. A* with the Misplaced Tile heuristic.")
+        algorithmType = int(raw_input("3. A* with the Manhattan distance heuristic.\n"))
+        if algorithmType == 1:
+            Unifor_Cost_Search(problm)
+
 
     elif ValInput == 2:
         print("Enter your puzzle, use a zero to represent the blank")
@@ -18,6 +42,11 @@ if __name__ == "__main__":
         rowOne = map(int, rowOne.split())
         rowTwo = map(int, rowTwo.split())
         rowTree = map(int, rowTree.split())
-
-        problem = (rowOne,rowTwo,rowTree)
-        print(problem)
+        m = (rowOne,rowTwo,rowTree)
+        problem = parent(m,0)
+        print("Enter your choice of algorithm")
+        print("1. Uniform Cost Search")
+        print("2. A* with the Misplaced Tile heuristic.")
+        algorithmType = int(raw_input("3. A* with the Manhattan distance heuristic.\n"))
+        if algorithmType == 1:
+            Unifor_Cost_Search(problm)
