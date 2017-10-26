df026ee1c05edb034ae7cdb05740a9042a582617
Marco Mercado <marco_mercado@outlook.com>
2017-10-26 16:02:00 -0700

    fix input matrix

--- puz.py
+++ puz.py
@@ -1,21 +1,23 @@
 import sys
-import numpy
-
-
-
-
+import numpy as np
 
 
 
 if __name__ == "__main__":
-    problem
+
     print("Welcome to Bertie Woosters 8-puzzle solver. ")
-    ValInput = int(input("Type “1” to use a default puzzle, or “2” to enter your own puzzle." ))
+    ValInput = input("Type \"1\" to use a default puzzle, or \"2\" to enter your own puzzle\n" )
     if ValInput ==  1:
-        problem = numpy.matrix('1,2,3;4,5,6;7.8,0')
+        problem = np.matrix([[1,2,3], [4,5,6], [7.8,0]])
+
     elif ValInput == 2:
         print("Enter your puzzle, use a zero to represent the blank")
-        rowOne = input("Enter the first row, use space or tabs between numbers" )
-        rowTwo = input("Enter the second row, use space or tabs between numbers")
-        rowTree = input("Enter the third row, use space or tabs between numbers")
-        problme = (rowOne,rowTwo, rowTree)
+        rowOne = raw_input("Enter the first row, use space or tabs between numbers\n" )
+        rowTwo = raw_input("Enter the second row, use space or tabs between numbers\n")
+        rowTree = raw_input("Enter the third row, use space or tabs between numbers\n")
+        rowOne = map(int, rowOne.split())
+        rowTwo = map(int, rowTwo.split())
+        rowTree = map(int, rowTree.split())
+
+        problem = (rowOne,rowTwo,rowTree)
+        print(problem)
